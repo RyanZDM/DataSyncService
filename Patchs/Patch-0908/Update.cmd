5c7a00f7aa03b3b8d5724b01bcad67f2c3f3466f
Ryan Zhang <dameng.zhang@carestream.com>
2017-09-07 17:20:38 +0800

    Refined the trigger, added a path for correcting the value of Runtime

---
+++ Patchs/Patch-0908/Update.cmd
@@ -0,0 +1,14 @@
+@echo off
+cd /d "%~dp0"
+
+Echo Updating database...
+sqlcmd -E -d OPC -S .\WINCC -i tr_UpdateItemSubtotal.sql -o c:\log\Patch0908.log
+
+sqlcmd -E -d OPC -S .\WINCC -i UpdateParams.sql -o c:\log\Patch0908.log
+
+@echo Patch applied. >> C:\log\Patch0908.log
+
+Echo Patch applied.
+Pause
+
+:End
---
+++ Patchs/Patch-0908/UpdateParams.sql
@@ -0,0 +1,4 @@
+
+Update [ShiftStatDet] Set SubTotalBegin=SubTotalLast - 12 Where Item = 'SubtotalRuntime1' and ShiftId='B13BB5F4-97E2-4CC9-94FC-986387A00A81'
+
+Update [ShiftStatDet] Set SubTotalBegin=SubTotalLast - 12 Where Item = 'SubtotalRuntime2' and ShiftId='B13BB5F4-97E2-4CC9-94FC-986387A00A81'
